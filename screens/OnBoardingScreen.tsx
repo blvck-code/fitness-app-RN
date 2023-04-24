2320895364d5276304d218a3b72e93943d36f19e
maurice <maurice.oluoch@naiparq.co.ke>
2023-04-24 17:26:50 +0300

    first commit

---
+++ screens/OnBoardingScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+
+const OnBoardingScreen = () => {
+  return (
+    <ImageBackground>
+      <Text style={{ color: "#fff" }}>Working</Text>
+    </ImageBackground>
+  );
+};
+
+export default OnBoardingScreen;
---
+++ screens/PlanOverviewScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PlanOverviewScreen = () => {
+  return (
+    <View>
+      <Text>PlanOverviewScreen</Text>
+    </View>
+  );
+};
+
+export default PlanOverviewScreen;
+
+const styles = StyleSheet.create({});
